55193811ff14397a817767af400673264c753e04
ehh99 <jinung5@gmail.com>
2024-07-06 16:13:19 +0900

    init: 초기설정

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // src 폴더를 루트 디렉토리로 설정
+  root: 'src',
+  resolve: {
+    // @ 기호를 사용하여 src 폴더의 경로를 별칭으로 설정
+    alias: { '@': resolve(__dirname, 'src') },
+  },
+  build: {
+    // 빌드 결과물이 저장될 폴더를 dist로 설정
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        // 기본 입력 파일을 설정 (index.html이 src 폴더 내에 위치한다고 가정)
+        main: resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
+  server: {
+    // 개발 서버 설정
+    port: 3000, // 개발 서버 포트 번호를 3000으로 설정
+    open: false, // 서버 시작 시 브라우저 자동 열기
+  },
+  css: {
+    // CSS 전처리기 옵션 설정
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`, // SCSS 변수 파일을 모든 SCSS 파일에 자동으로 포함
+      },
+      modules: {
+        scopeBehaviour: 'local', // CSS Modules를 사용하여 CSS의 범위를 모듈 단위로 제한
+      },
+    },
+  },
+  plugins: [],
+});
