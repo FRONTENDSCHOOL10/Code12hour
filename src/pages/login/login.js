4ba9648afb8c7516c8cd62a06f31597cb6d6e336
dongho <ldd0702@naver.com>
2024-07-09 13:26:59 +0900

    feature : 로그인, 회원가입 페이지 마크업
    - 로그인, 회원가입 페이지 마크업
    - 로그인, 회원가입 페이지 스타일링
    - 회원가입 페이지 이메일 인증번호 input placeholder 수정
    - 로그인,회원가입 페이지 헤더 풋터 연결완료
    - 에셋 아이콘 체크박스, 라디오 svg 파일 추가
    
    Resolves #3

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,1 +1,18 @@
 import './login.scss';
+
+import { defineCustomElements } from '/utils/index';
+import { footer, header } from '/components/index';
+
+// const app = document.getElementById('app');
+
+const init = () => {
+  defineCustomElements([
+    ['c-header', header],
+    ['c-footer', footer],
+  ]);
+
+  // appendCustomElement(app, 'c-header');
+  // appendCustomElement(app, 'c-footer');
+};
+
+init();
