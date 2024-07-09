15f0671c5cd131934b76ea62180cde2089811e21
ehh99 <jinung5@gmail.com>
2024-07-09 09:51:01 +0900

    feat: footer 컴포넌트 마크업 - footer 마크업 및 컴포넌트 분리 완료 - header 컴포넌트 box-shadow 추가 및 기타 스타일링 수정 - prdouct-detail 페이지 header, footer 컴포넌트 추가 - mixin flex 추가, effects 전역변수 추가 Resolve #7

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
 ];
