23e4608c6057639ac3188a97f4cbeaacae3388e5
dongho <ldd0702@naver.com>
2024-07-12 13:43:34 +0900

    feature: 상품 리스트 사이드 필터 패널 마크업
    - main 태그 삭제
    - nav 태그 삭제
    - 카테고리 헤더 영역 수정
    
    - html 수정 되면서 변경된 스타일링 수정
    
    - 카테고리 내 input 선택 시 카테고리 헤더 카운트 기능 구현
    
    - 카테고리 헤더 영역 p 태그 aria-hidden 처리
    
    - mixin font 추가

--- src/components/side-filter-panel/side-filter-panel.js
+++ src/components/side-filter-panel/side-filter-panel.js
@@ -1,20 +1,43 @@
 import './side-filter-panel.scss';
 
+//체크박스 버튼 카운트
 document.addEventListener('DOMContentLoaded', function () {
-  const buttons = document.querySelectorAll('.category-toggle');
+  const updateCount = (detailsId) => {
+    const details = document.getElementById(detailsId);
+    const countSpan = details.querySelector('.header-count');
+    const checkboxes = details.querySelectorAll('.filter-panel__category-input');
+    let checkedCount = 0;
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        console.log(details);
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', function () {
-      const item = this.parentElement;
-      const isActive = item.classList.contains('category-header--active');
-
-      if (isActive) {
-        item.classList.remove('category-header--active');
-        this.classList.remove('active');
-      } else {
-        item.classList.add('category-header--active');
-        this.classList.add('active');
+        checkedCount++;
       }
+    });
+    countSpan.textContent = checkedCount;
+  };
+
+  document.querySelectorAll('.filter-panel__category-input').forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+      const parentDetails = checkbox.closest('.filter-panel__category-list').id;
+      updateCount(parentDetails);
+    });
+  });
+});
+
+// 라디오 버튼 카운트
+document.querySelectorAll('.filter-panel__category-list').forEach(function (categoryList) {
+  const inputs = categoryList.querySelectorAll(
+    '.filter-panel__category-input, .filter-panel__category-radio'
+  );
+  const headerCountSpan = categoryList.querySelector('.header-count');
+
+  inputs.forEach(function (input) {
+    input.addEventListener('change', function () {
+      const checkedCount = categoryList.querySelectorAll(
+        '.filter-panel__category-input:checked, .filter-panel__category-radio:checked'
+      ).length;
+      headerCountSpan.textContent = checkedCount;
     });
   });
 });
