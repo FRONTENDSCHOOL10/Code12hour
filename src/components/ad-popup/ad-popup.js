b26c003e8ce53fa6870da71db5e8f780ee150a60
happyhye <50566@naver.com>
2024-07-14 18:15:17 +0900

    feat: 광고 팝업창 '오늘 하루 안 보기' 기능 구현
    - 이미지를 정적파일로 분류
    - 로컬 스토리지를 사용하여 '오늘 하루 안 보기' 버튼 클릭 시 24시간 동안 팝업창 띄우지 않는 기능 구현
    
    Resolves #28

--- src/components/ad-popup/ad-popup.js
+++ src/components/ad-popup/ad-popup.js
@@ -5,15 +5,14 @@ const adPopupTemplate = document.createElement('template');
 adPopupTemplate.innerHTML = `
   <style>${css}</style>
   <div class="ad-popup" role="dialog" aria-modal="true" aria-labelledby="ad-popup__title">
-    
-    <a href="#" tabindex="0">
-      <img
-      class="ad-popup__img"
-      src="/src/assets/images/ad-popup/ad-popup.webp"
-      alt="처음 만나는 뷰티 플랫폼, 뷰티컬리"
-      />
-    </a>
-    <h1 class="sr-only" id="ad-popup__title">처음 만나는 뷰티 플랫폼, 뷰티컬리</h1>
+      <a href="#" tabindex="0">
+        <img
+          class="ad-popup__img"
+          src="/assets/images/ad-popup/ad-popup.webp"
+          alt="처음 만나는 뷰티 플랫폼, 뷰티컬리"
+        />
+      </a>
+      <h1 class="sr-only" id="ad-popup__title">처음 만나는 뷰티 플랫폼, 뷰티컬리</h1>
 
       <div class="ad-popup-button-group">
         <button class="ad-popup__today-close" type="button">오늘 하루 안 보기</button>
@@ -27,28 +26,48 @@ export class AdPopup extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(adPopupTemplate.content.cloneNode(true));
+
+    this.closeTime = localStorage.getItem('mainPopup'); // 로컬 스토리지 값 가져옴
   }
 
   connectedCallback() {
+    if (this.closeTime) {
+      this.checkedPopup();
+    }
     this.closeButtonEvent();
   }
 
   closeButtonEvent() {
     const button = this.shadowRoot.querySelectorAll('button');
-    const popup = this.shadowRoot.querySelector('.ad-popup');
 
     const handleCloseButton = (e) => {
       const target = e.target;
 
       if (target.classList.contains('ad-popup__today-close')) {
-        //TODO: 24시간 동안 팝업 안 보는 로직 구현하기
-        popup.remove();
+        this.setCloseTime();
+        this.remove();
       }
       if (target.classList.contains('ad-popup__close')) {
-        popup.remove();
+        this.remove();
       }
     };
 
     button.forEach((button) => button.addEventListener('click', handleCloseButton));
+  }
+
+  setCloseTime() {
+    const currentTime = Date.now(); // 현재 시간 가져옴
+    const oneDayInMillis = 24 * 60 * 60 * 1000; // 24시간을 ms로 변환
+    const closeTime = currentTime + oneDayInMillis;
+    localStorage.setItem('mainPopup', closeTime); // 로컬 스토리지에 저장
+  }
+
+  checkedPopup() {
+    const currentTime = Date.now(); // 현재 시간 가져옴
+    if (currentTime < this.closeTime) {
+      this.remove();
+    } else {
+      localStorage.removeItem('mainPopup');
+    }
   }
 }
